2178578a492040b2c0cc36e4cb76e3deca9cea96
TerroKot <egor.tereshonok@gmail.com>
2020-08-29 20:35:54 +0300

    add force

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,12 +2,15 @@
 
 public class PlayerMovement : MonoBehaviour {
     // Start is called before the first frame update
-    
+    public Rigidbody rb;
     void Start() {
         Debug.Log("Hello World");
+
+       // rb.AddForce(0, 200, 500);
     }
 
     // Update is called once per frame
-    void Update() {
+    void FixedUpdate() {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
     }
 }
