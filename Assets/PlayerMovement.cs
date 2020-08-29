258092ecfbaa1e43c52dd49dd72a3793ee897983
TerroKot <egor.tereshonok@gmail.com>
2020-08-30 00:12:22 +0300

    add controllers

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,6 +3,10 @@
 public class PlayerMovement : MonoBehaviour {
     // Start is called before the first frame update
     public Rigidbody rb;
+
+    public float forwardForce  = 2000f;
+    public float sidewaysForce = 500f;
+    
     void Start() {
         Debug.Log("Hello World");
 
@@ -11,6 +15,14 @@ public class PlayerMovement : MonoBehaviour {
 
     // Update is called once per frame
     void FixedUpdate() {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        // Add forward forse 
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d")) {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a")) {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
