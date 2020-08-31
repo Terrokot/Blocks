8ef03d945bde0475de1d3c175de50732db7e6e5a
TerroKot <egor.tereshonok@gmail.com>
2020-09-01 01:05:31 +0300

    add score

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.CompareTag("Obstacle"))
+        {
+            Debug.Log("We hit an obsticle");
+            movement.enabled = false;
+        }
+    }
+}
+
